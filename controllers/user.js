f9d00eb020c8c0800a9af968a4c507de048c991b
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-04-11 22:48:49 +0530

    signin

--- controllers/user.js
+++ controllers/user.js
@@ -14,9 +14,9 @@ export const signin = async (req, res) => {
     if (!oldUser) 
     return res.status(404).json({ message: "User doesn't exist" });
 
-    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+    // const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-    if (!isPasswordCorrect) {
+    if (password!==oldUser.password) {
     console.log("wrong");
     return res.status(400).json({ message: "Invalid credentials" });}
 
@@ -28,41 +28,3 @@ export const signin = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
-
-
-// sorry for the trouble
-export const signup = async(req,res)=>{
-    const {firstName,lastName,email,password}=obj;
-    console.log(email);
-    
-    try {
-        
-        const existingUser= await User.findOne({email:email});
-        
-        // console.log(email); 
-        if(existingUser)
-        {
-            return res.status(400).json({message:"User already exist"});
-        }
-        if(password!==confirmPassword)
-        {
-            
-            return res.status(400).json({message:"Password Doesn't Match"});
-        }
-        // console.log(firstName);
-        const hashedPassword=await bcrypt.hash(password,10);
-            // console.log(hashedPassword);
-            const result =await User.create({email,password:hashedPassword,name:`${firstName} ${lastName}`,});
-            
-        // const token = jwt.sign({email:existingUser.email,id:existingUser._id},'test',{expiresIn:"1h"});
-        // res.status(200).json({result,token});
-
-        const token = jwt.sign( { email: result.email, id: result._id },'test', { expiresIn: "1h" } );
-        
-        res.status(201).json({ result,token });
-    } catch (error) {
-        
-        res.status(500).json({message:"something went wrong"});
-    }
-
-}
