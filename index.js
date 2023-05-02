c6631a9a83a4e5872e1237753c3004e5affb439e
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-05-02 19:27:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import { fetchData } from "./api/fetch.js";
 import userRoutes from "./routes/users.js";
 import user from "./models/user.js";
+import http from 'node:http';
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
