51f8f6f6cfc3503613aee43d3082b512992e7f97
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-05-03 19:43:34 +0530

    fix vercel2

--- app.js
+++ app.js
@@ -25,36 +25,36 @@ app.use("/user", userRoutes);
 //   )
 //   .catch((error) => console.log(`${error} did not connected`));
 
-setInterval(() => {
-  let obj = [];
-  fetchData("user").then((data) => {
-    obj = data;
+// setInterval(() => {
+//   let obj = [];
+//   fetchData("user").then((data) => {
+//     obj = data;
 
-    for (let x of obj) {
-      // console.log(x);
-      if (x.allowed === true) {
-        const insertUser = async () => {
-          const check = await user.findOne({ email: x.email });
-          if (!check) {
-            const res = await user.create({
-              email: x.email,
-              password: x.password,
-              name: `${x.firstName} ${x.lastName}`,
-            });
-            // console.log(res);
-          }
-        };
-        insertUser();
-      } else {
-        const deleteUser = async () => {
-          const res = await user.deleteMany({ email: x.email });
-          // console.log(res);
-        };
-        deleteUser();
-      }
-    }
-  });
-}, 10000);
+//     for (let x of obj) {
+//       // console.log(x);
+//       if (x.allowed === true) {
+//         const insertUser = async () => {
+//           const check = await user.findOne({ email: x.email });
+//           if (!check) {
+//             const res = await user.create({
+//               email: x.email,
+//               password: x.password,
+//               name: `${x.firstName} ${x.lastName}`,
+//             });
+//             // console.log(res);
+//           }
+//         };
+//         insertUser();
+//       } else {
+//         const deleteUser = async () => {
+//           const res = await user.deleteMany({ email: x.email });
+//           // console.log(res);
+//         };
+//         deleteUser();
+//       }
+//     }
+//   });
+// }, 10000);
 
 
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology: true});
